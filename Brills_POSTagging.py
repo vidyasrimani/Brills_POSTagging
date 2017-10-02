ddbaf0ed00c803434c1f7402b44cf65fc5526c49
Vidya Mani <sm.vidya@gmail.com>
2017-10-01 23:02:41 -0500

    Most Probable Tag recomputed

--- Brills_POSTagging.py
+++ Brills_POSTagging.py
@@ -7,6 +7,8 @@
 from collections import Counter
 
 testSentence = "The president wants to control the board 's control"
+correctTestSentenceTagged = """The_DT president_NN wants_VBZ to_TO
+control_VB the_DT board_NN 's_POS control_NN"""
 
 #writes the Probability to specified file
 def writeToFile(filename,Lst):
@@ -19,67 +21,54 @@ def getTagCount(file):
     WordTagCount={}
     WordTagProb = {}
     WordCount={}
+    allWords = []
+    allTags = []
+
     print('Getting Tag count...')
     content = open(file).read()
 
-    allWords=content.split()#split all words by space
-    totalNumberOfWords= len(allWords)
-
-    #For each word get the word and tag which is joined by '_'
-    for wordTag in allWords:
-        word,tag = wordTag.split('_')#every word is joined by '_'
-        if word not in WordCount.keys():
-            WordCount[word]=1
+    allWord=content.split()#split all words by space
+    totalNumberOFWords = len(allWord)#Total Number of words
+    for wordTag in allWord:
+        word = wordTag.split('_')
+        if(len(word)>1):
+            allWords.append(word[0])
+            allTags.append(word[1])
         else:
-            WordCount[word] += 1#inclement the word tag count
+            allWords.append(word[0])
+            allTags.append('')
 
-        if word not in WordTagCount.keys():
-            WordTagCount[word] = {}#every entry will have a word and tag
-            WordTagCount[word][tag] = 1
+    #tag word count
+    tagWordCount = Counter(zip(allWords,allTags))
+    tagWordCount = sorted(tagWordCount.items(), key=lambda x:x[1],reverse=True)
+
+    mostProbableTag ={}
+    for wordTag in tagWordCount:
+        if(len(wordTag)>1):
+            w=wordTag[0][0] #word
+            t=wordTag[0][1] #tag
+            c=wordTag[1]    #count
         else:
-            if tag not in WordTagCount[word]:
-                WordTagCount[word][tag] = 1
-            else:
-                WordTagCount[word][tag] += 1
+            w=word[0]
+            t = ''
+            c = 1
+        if wordTag[0] not in mostProbableTag:
+            mostProbableTag[wordTag[0]] = wordTag[1]
+        else:
+            if(wordTag[1]>mostProbableTag[wordTag[0]]):
+                mostProbableTag[wordTag[0]] = wordTag[1]
 
-    #Getting most probable tag
-    print('Finding Most Probable Tag...')
-    mostProbTag = {}
-    for key,value in WordTagCount.items():
-        mostProbTag[key]=max(value,key=value.get)
 
-    #The probability of the most probable tag is calculated
-    for word,tag in mostProbTag.items():
-        pWord = WordCount[word]/totalNumberOfWords
-        pMostProbability = WordTagCount[word][tag]/totalNumberOfWords
-        if word not in WordTagProb.keys():
-            WordTagProb[word] = {}
-            WordTagProb[word][tag] = pMostProbability/pWord
 
-    #Write the unigram model probability for most probable tag
-    filename = 'Q3-UnigramMostProbableTag.txt'
-    writeToFile(filename,WordTagProb)
-    print('Unigram Most Probable POS Probability written to file!')
-    print('------------------------------------------------------')
 
-    #Calculating probability without Brill's transformation
-    sentProbability = 1
-    testSentenceWords = testSentence.split()
 
-    for w in testSentenceWords:
-        if (w in WordTagProb.keys()):
-            wTagProbKey = WordTagProb[w]
-            wTag=wTagProbKey.keys()
-            wProb=str(wTagProbKey.values())
-            str1=wProb.split('[')
-            str2=str1[1].split(']')
-            wFloatProb = float(str2[0])
-            #wTag,wProb = WordTagProb[w]#getting the tag, and probabilty of the word from test sentence
-            sentProbability *= wFloatProb
-            print('Here')
 
-    print("Probabilty of sentence before Brill's transformation : ")
-    print(sentProbability)
+    print('OKay')
+
+
+
+
+
 
 
 
